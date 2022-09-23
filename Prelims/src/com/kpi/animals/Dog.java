1417a7923c4d9217e85ba138377bc29d44dda325
NidhishreeLA2 <nidhishree.la@kpipartners.com>
2022-09-23 15:30:40 +0530

    class commit

---
+++ Prelims/src/com/kpi/animals/Dog.java
@@ -0,0 +1,10 @@
+package com.kpi.animals;
+
+
+//public class Dog extends Animal
+//dog is omnivores : is a relationship - inheritance
+// child - dog , parent - omnivores
+//extends = binds child with parent
+public class Dog extends Omnivores {
+
+}
